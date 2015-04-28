9dad73ab7b18528753eb2476acd789242dfb05c1
Michaela McCann <kaebot@me.com>
2015-04-28 13:06:26 -0700

    implemented wide layout for hero

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
 <section class="bg-yellow" id="hero">
 	<div class="container">
-		<img id="hero-logo" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="A logo with scaffolding being designed."><br>
+		<div class="gutter">
+			<img id="hero-logo" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="A logo with scaffolding being designed.">
+		</div>
+		<div class="content">
 		<h1>Layers</h1>
 		<div id="hero-info">
-			<p id="hero-city">San Francisco, CA</p>
-			<p aria-hidden="true" id="hero-dates">_JUN 08-12</p>
+			<p class="hero-detail" id="hero-city">San Francisco, CA</p>
+			<p aria-hidden="true" class="hero-detail" id="hero-dates">_ JUN 08-10</p>
 			<p class="sr">June 08-12</p>
 		</div>
 		<h2>A new design conference for the Mac+iOS community</h2>
