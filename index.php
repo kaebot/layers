22b1f8f55a110d42db1c520fbb1101ccbc0ec691
Michaela McCann <kaebot@me.com>
2015-04-27 18:40:30 -0700

    markup for hero

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
 
+<section id="hero">
+	<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="A logo with scaffolding being designed." id="header-logo"><br>
+	<h1>Layers</h1>
+	<div id="hero-info">
+		<p id="hero-city">San Francisco, CA</p>
+		<p aria-hidden="true" id="hero-dates">_JUN 08-12</p>
+		<p class="sr">June 08-12</p></div>
+		<h2>A new design conference for the Mac+iOS community</h2>
+</section>
 
 <?php get_footer(); ?>
